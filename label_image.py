9372a85a7a037ac88cfee9285ffbaead941dbbeb
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2020-10-26 17:04:25 +0100

    update

--- label_image.py
+++ label_image.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 
 def load_graph(model_file):
     graph=tf.Graph()
-    graph_def = tf.GraphDef()
+    graph_def = tf.compat.v1.GraphDef()
 
     with open(model_file,"rb") as f:
         graph_def.ParseFromString(f.read())
@@ -53,69 +53,74 @@ def load_labels(label_file):
 
 
 def main(img):
-    file_name = img
-    model_file = "retrained_graph.pd"
-    label_file = "retrained_labels.txt"
-    input_height = 224
-    input_width = 224
-    input_mean = 128
-    input_std = 128
-    input_layer = "input"
-    output_layer = "final_result"
+  file_name = img
+  model_file = "retrained_graph.pb"
+  label_file = "retrained_labels.txt"
+  input_height = 224
+  input_width = 224
+  input_mean = 128
+  input_std = 128
+  input_layer = "input"
+  output_layer = "final_result"
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--image",help="image to processed")
-    parser.add_argument("--graph", help="graph/model to be executed")
-    parser.add_argument("--labels", help="name of file containing labels")
-    parser.add_argument("--input_height", type=int,help="input width")
-    parser.add_argument("--input_mean", type=int, help="input mean")
-    parser.add_argument("--input_std",type=int, help="input std")
-    parser.add_argument("--input_layer", help="name of input layer")
-    parser.add_argument("--output_layer", help="name of output layer")
-    args = parser.parse_args()
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--image", help="image to be processed")
+  parser.add_argument("--graph", help="graph/model to be executed")
+  parser.add_argument("--labels", help="name of file containing labels")
+  parser.add_argument("--input_height", type=int, help="input height")
+  parser.add_argument("--input_width", type=int, help="input width")
+  parser.add_argument("--input_mean", type=int, help="input mean")
+  parser.add_argument("--input_std", type=int, help="input std")
+  parser.add_argument("--input_layer", help="name of input layer")
+  parser.add_argument("--output_layer", help="name of output layer")
+  args = parser.parse_args()
 
-    if args.graph:
-        model_file = args.graph
-    if args.image:
-        file_name = args.image
-    if args.labels:
-        label_file = args.labels
-    if args.input_height:
-        input_height = args.input_height
-    if args.input_width:
-        input_width = args.input_width
-    if args.input_mean:
-        input_mean = args.input_mean
-    if args.input_std:
-        input_std = args.input_std
-    if args.input_layer:
-        input_layer = args.input_layer
-    if args.output_layer:
-        output_layer = args.output_layer
+  if args.graph:
+    model_file = args.graph
+  if args.image:
+    file_name = args.image
+  if args.labels:
+    label_file = args.labels
+  if args.input_height:
+    input_height = args.input_height
+  if args.input_width:
+    input_width = args.input_width
+  if args.input_mean:
+    input_mean = args.input_mean
+  if args.input_std:
+    input_std = args.input_std
+  if args.input_layer:
+    input_layer = args.input_layer
+  if args.output_layer:
+    output_layer = args.output_layer
+
+  graph = load_graph(model_file)
+  t = read_tensor_from_image_file(file_name,
+                                  input_height=input_height,
+                                  input_width=input_width,
+                                  input_mean=input_mean,
+                                  input_std=input_std)
+
+  input_name = "import/" + input_layer
+  output_name = "import/" + output_layer
+  input_operation = graph.get_operation_by_name(input_name);
+  output_operation = graph.get_operation_by_name(output_name);
 
 
-    graph = load_graph(model_file)
+  with tf.Session(graph=graph) as sess:
+    start = time.time()
+    results = sess.run(output_operation.outputs[0],
+                      {input_operation.outputs[0]: t})
+    end=time.time()
+  results = np.squeeze(results)
 
-    t = read_tensor_from_image_file(file_name,
-                                    input_height=input_height,
-                                    input_width=input_width,
-                                    input_mean= input_mean,
-                                    input_std=input_std)
+  top_k = results.argsort()[-5:][::-1]
+  labels = load_labels(label_file)
 
-    input_name = "import/" + input_layer
-    output_name = "import/" + output_layer
-    input_operation = graph.get_operation_by_name(input_name)
-    output_operation = graph.get_operation_by_name(output_name)
 
-    with tf.Session(graph=graph) as sess:
-        start = time.time()
-        results = sess.run(output_operation.outputs[0],
-                           {input_operation.outputs[0]: t})
-        end = time.time()
-    results = np.squeeze(results)
 
-    top_k = results.argsort()[-5:][::-1]
-    labels = load_labels(label_file)
 
-    for i in top_k:
-        return labels[i]
+
+
+  for i in top_k:
+    return labels[i]
--- music_player.py
+++ music_player.py
@@ -3,21 +3,124 @@ import cv2
 import label_image
 import os,random
 import subprocess
+import vlc
+import pafy
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 size = 4
 
 # let's load the xml
 classifier = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 global text
-webcame = cv2.VideoCapture(0)
+webcam = cv2.VideoCapture(0)
 now = time.time()
 future = now + 60
 while True:
-    (rval,im) = webcame.read()
-    im = cv2.flip(im,1,0)
+    (rval, im) = webcam.read()
+    im = cv2.flip(im, 1, 0)  # Flip to act as a mirror
     # Resize the image to speed up detection
     mini = cv2.resize(im, (int(im.shape[1] / size), int(im.shape[0] / size)))
+    # detect MultiScale / faces
     faces = classifier.detectMultiScale(mini)
+    # Draw rectangles around each face
+    for f in faces:
+        (x, y, w, h) = [v * size for v in f]  # Scale the shapesize backup
+        sub_face = im[y:y + h, x:x + w]
+        FaceFileName = "test.jpg"  # Saving the current image from the webcam for testing.
+        cv2.imwrite(FaceFileName, sub_face)
+        text = label_image.main(FaceFileName)  # Getting the Result from the label_image file, i.e., Classification Result.
+        text = text.title()  # Title Case looks Stunning.
+        font = cv2.FONT_HERSHEY_TRIPLEX
 
-    # Draw rectangle around each face
+        if text == 'Angry':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 25, 255), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0, 25,255), 2)
 
+        if text == 'Smile':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0,260,0), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0,260,0), 2)
+
+        if text == 'Fear':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 255), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0, 255, 255), 2)
+
+        if text == 'Sad':
+            cv2.rectangle(im, (x, y), (x + w, y + h), (0,191,255), 7)
+            cv2.putText(im, text, (x + h, y), font, 1, (0,191,255), 2)
+
+    # Show the image/
+    cv2.imshow('Music player with Emotion recognition', im)
+    key = cv2.waitKey(30)& 0xff
+    if time.time() > future:##after 20second music will play
+        try:
+            cv2.destroyAllWindows()
+            mp = 'C:/Program Files (x86)/Windows Media Player/wmplayer.exe'
+            if text == 'Angry':
+                AngryURL = "https://www.youtube.com/watch?v=FZLILV18ut8&list=PL_MH8gOS_ETiNT1NF8B46JYHZe6fXWfVW&index=6"
+                video = pafy.new(AngryURL)
+                best = video.getbest()
+                playurl = best.url
+                Instance = vlc.Instance()
+                player = Instance.media_player_new()
+                Media = Instance.media_new(playurl)
+                Media.get_mrl()
+                player.set_media(Media)
+                player.play()
+                # randomfile = random.choice(os.listdir("/home/eddie/Desktop/python/music_player_with_emotions/songs/Angry/"))
+                # print('You are angry !!!! please calm down:) ,I will play song for you :' + randomfile)
+                # file = ('/home/eddie/Desktop/python/music_player_with_emotions/songs/Angry/' + randomfile)
+                # subprocess.call([mp, file])
+            if text == 'Smile':
+                SmileURL = "https://www.youtube.com/watch?v=vZA5heWazIQ"
+                video = pafy.new(SmileURL)
+                best = video.getbest()
+                playurl = best.url
+                Instance = vlc.Instance()
+                player = Instance.media_player_new()
+                Media = Instance.media_new(playurl)
+                Media.get_mrl()
+                player.set_media(Media)
+                player.play()
+                # randomfile = random.choice(os.listdir("/home/eddie/Desktop/python/music_player_with_emotions/songs/Smile/"))
+                # print('You are smiling :) ,I playing special song for you: ' + randomfile)
+                # file = ('/home/eddie/Desktop/python/music_player_with_emotions/songs/Smile/' + randomfile)
+                # subprocess.call([mp, file])
+
+            if text == 'Fear':
+                FearURL = "https://www.youtube.com/watch?v=q-wGMlSuX_c"
+                video = pafy.new(FearURL)
+                best = video.getbest()
+                playurl = best.url
+                Instance = vlc.Instance()
+                player = Instance.media_player_new()
+                Media = Instance.media_new(playurl)
+                Media.get_mrl()
+                player.set_media(Media)
+                player.play()
+                # randomfile = random.choice(os.listdir("/home/eddie/Desktop/python/music_player_with_emotions/songs/songs/Fear/"))
+                # print('You have fear of something ,I playing song for you: ' + randomfile)
+                # file = ('/home/eddie/Desktop/python/music_player_with_emotions/songs/Fear/' + randomfile)
+                # subprocess.call([mp, file])
+
+            if text == 'Sad':
+                FearURL = "https://www.youtube.com/watch?v=BZsXcc_tC-o"
+                video = pafy.new(FearURL)
+                best = video.getbest()
+                playurl = best.url
+                Instance = vlc.Instance()
+                Sadplayer = Instance.media_player_new()
+                Media = Instance.media_new(playurl)
+                Media.get_mrl()
+                Sadplayer.set_media(Media)
+                Sadplayer.play()
+                # randomfile = random.choice(os.listdir("/home/eddie/Desktop/python/music_player_with_emotions/songs/Sad/"))
+                # print('You are sad,dont worry:) ,I playing song for you: ' + randomfile)
+                # file = ('/home/eddie/Desktop/python/music_player_with_emotions/songs/Sad/' + randomfile)
+                # subprocess.call([mp, file])
+            break
+
+        except :
+            print('Please stay focus in Camera frame atleast 15 seconds & run again this program:)')
+            break
+
+    if key == 27:  # The Esc key
+        break
