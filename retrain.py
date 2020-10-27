d7375974acc9ce2b5e38d7604e30ca6ab7879bf1
peyman iravani <peyman.iravani.altranitalia@windtre.it>
2020-10-27 16:38:42 +0100

    update - finish the code and successful run

--- retrain.py
+++ retrain.py
@@ -1327,5 +1327,5 @@ if __name__ == '__main__':
       for more information on Mobilenet.\
       """)
   FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
   #tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
